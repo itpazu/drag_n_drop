16d4ba275733f7ad4b7c5b910cd84efd752475ae
itpazu <itpazu@gmail.com>
2023-04-04 23:15:23 +0300

    migration to postgress

--- cannabis/settings.py
+++ cannabis/settings.py
@@ -27,7 +27,7 @@ print(os.environ.get('SECRET_KEY'))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False if os.getenv('ENV') == 'production' else True
+DEBUG = False if os.getenv('ENVIRONMENT') == 'PRODUCTION' else True
 
 ALLOWED_HOSTS = [".vercel.app", "localhost"]
 
@@ -80,9 +80,14 @@ WSGI_APPLICATION = 'cannabis.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.environ.get('DB_NAME'),
+        "USER": os.environ.get('DB_USER'),
+        "PASSWORD": os.environ.get('DB_PASSWORD'),
+        "HOST": os.environ.get('HOST'),
+        "PORT": os.environ.get('PORT'),
+
     }
 }
 
