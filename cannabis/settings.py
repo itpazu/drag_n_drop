209cb28016adc9663ad5d72d6bd8b0c38a1fcc58
itpazu <itpazu@gmail.com>
2023-04-04 18:28:27 +0300

    fixing requirements and settings

--- cannabis/settings.py
+++ cannabis/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4erjknbg=n4s=63ffkf4wogqt+vwgq$jl84=&l(g1*$*5rp3uu'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False if os.getenv('ENV') == 'production' else True
 
 ALLOWED_HOSTS = [".vercel.app"]
 
